a900fd072aeb1d4fa38f0f63e1e337ef2a0a892a
Nephty <211260@umons.ac.be>
2022-12-02 14:29:09 +0100

    added main and cmake

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nephty on 12/2/22.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Nephty on 12/2/22.
+//
+
+#ifndef RINGSTAR_MAIN_H
+#define RINGSTAR_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //RINGSTAR_MAIN_H
