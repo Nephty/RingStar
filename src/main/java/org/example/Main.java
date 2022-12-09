465aa7ce8b828a517a36f7c426cba0329575be30
Thomas <thomas.bernard@student.umons.ac.be>
2022-12-09 17:47:53 +0100

    Main modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import org.example.thomas.RandomShit;
+
+import java.io.FileNotFoundException;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MatrixReader mr = new MatrixReader("src/main/resources/data1.dat" /*+ args[0]*/);
+        try{
+            mr.matrixRead();
+        }catch (FileNotFoundException e){
+            e.getMessage();
+        }
+
+        // Choisir le solver à appeler
+        //RandomShit solver = new RandomShit(mr);
+
+        //Solution solution = solver.solve(mr.ringCost, mr.starCost, mr.length_of_matrix);
+
     }
+
+
+
+
 }
