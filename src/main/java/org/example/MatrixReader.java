8d6eaf397fa0cdbb98550f067300b6c61031ed38
Lorzi <lolokachu@gmail.com>
2022-12-09 17:31:34 +0100

    Rajout du try/catch dans le MatrixReader.

--- src/main/java/org/example/MatrixReader.java
+++ src/main/java/org/example/MatrixReader.java
@@ -22,36 +22,40 @@ public class MatrixReader {
      * @throws FileNotFoundException
      */
     public void matrixRead() throws FileNotFoundException {
-        File file = new File(this.path);
-        Scanner scanner = new Scanner(file);
-        this.length_of_matrix = scanner.nextInt();                          // le premier entier du fichier en entete nous donne la taille de la matrice
-        this.ringCost = new int[length_of_matrix][length_of_matrix];        // Première matrice du fichier
-        this.starCost = new int[length_of_matrix][length_of_matrix];        // Deuxième matrice du fichier
+        try{
+            File file = new File(this.path);
+            Scanner scanner = new Scanner(file);
+            this.length_of_matrix = scanner.nextInt();                          // le premier entier du fichier en entete nous donne la taille de la matrice
+            this.ringCost = new int[length_of_matrix][length_of_matrix];        // Première matrice du fichier
+            this.starCost = new int[length_of_matrix][length_of_matrix];        // Deuxième matrice du fichier
 
-        int count = 0;
-        int count2 = 0;
+            int count = 0;
+            int count2 = 0;
 
-        while(count2 != length_of_matrix){                                  //boucle qui transfert les élément de la premiere matrice du fichier .dat dans ringCost
+            while(count2 != length_of_matrix){                                  //boucle qui transfert les élément de la premiere matrice du fichier .dat dans ringCost
 
-            while(count != length_of_matrix){                               //ajoute l'un à la suite de l'autre les éléments de la 1ere matrice du fichier .dat dans ringCost
-                this.ringCost[count2][count] = scanner.nextInt();
-                count++;
+                while(count != length_of_matrix){                               //ajoute l'un à la suite de l'autre les éléments de la 1ere matrice du fichier .dat dans ringCost
+                    this.ringCost[count2][count] = scanner.nextInt();
+                    count++;
+                }
+                count2++;
+                count = 0;
             }
-            count2++;
             count = 0;
-        }
-        count = 0;
-        count2 = 0;
+            count2 = 0;
 
-        while(count2 != length_of_matrix){                                  //boucle qui transfert les élément de la deuxieme matrice du fichier .dat dans starCost
+            while(count2 != length_of_matrix){                                  //boucle qui transfert les élément de la deuxieme matrice du fichier .dat dans starCost
 
-            while(count != length_of_matrix){                               //ajoute l'un à la suite de l'autre les éléments de la 2eme matrice du fichier .dat dans starCost
-                this.starCost[count2][count] = scanner.nextInt();
-                count++;
+                while(count != length_of_matrix){                               //ajoute l'un à la suite de l'autre les éléments de la 2eme matrice du fichier .dat dans starCost
+                    this.starCost[count2][count] = scanner.nextInt();
+                    count++;
+                }
+                count2++;
+                count = 0;
             }
-            count2++;
-            count = 0;
         }
+        catch (FileNotFoundException e){System.out.println("That file does not exist, please verify the path");}
+
 
         //afficher la matrice 1 et 2 pour debug
         //System.out.println(ringCost[0][0]);
