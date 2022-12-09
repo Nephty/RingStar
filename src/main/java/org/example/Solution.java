e31cdc276cd5dd9b2c34ede53df62b988b47c0c9
kiyusa <aughouba@gmail.com>
2022-12-09 15:56:12 +0100

    Solution implemented

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,2 +1,30 @@
-package org.example;public class Solution {
+package org.example;
+
+public class Solution {
+    private int[] ring;
+    private int[][] star;
+    public Solution(int[] ring, int[][] star) {
+        this.ring = ring;
+        this.star = star;
+    }
+
+    public int cost() {
+        return 0;
+    }
+
+    public int[] getRing() {
+        return ring;
+    }
+
+    public void setRing(int[] ring) {
+        this.ring = ring;
+    }
+
+    public int[][] getStar() {
+        return star;
+    }
+
+    public void setStar(int[][] star) {
+        this.star = star;
+    }
 }
