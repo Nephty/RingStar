85c938ff3bf375eb047b57b03b30453ede6da7cc
kiyusa <aughouba@gmail.com>
2022-12-09 17:17:22 +0100

    neighbour finding method

--- src/main/java/grasp/Grasp.java
+++ src/main/java/grasp/Grasp.java
@@ -2,11 +2,18 @@ package grasp;
 
 import org.example.Solution;
 
+import java.util.ArrayList;
+
 public class Grasp {
     private final float ALPHA;
+    private final int [][] ringCostMatrix;
 
-    public Grasp(float alpha) {
+    private final int [][] starCostMatrix;
+
+    public Grasp(float alpha, int[][] ringCostMatrix, int[][] starCostMatrix) {
         this.ALPHA = alpha;
+        this.ringCostMatrix = ringCostMatrix;
+        this.starCostMatrix = starCostMatrix;
     }
 
     public Solution solve() {
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Solution {
     private ArrayList<Integer> ring;
@@ -12,6 +14,18 @@ public class Solution {
 
     public int cost() {
         return 0;
+    }
+
+    public void switchStarNode() {
+        final int starNodeToSwitch = ThreadLocalRandom.current().nextInt(0, star.size() + 1);
+        final Integer[] starEdge = star.get(starNodeToSwitch);
+        int newRingNode;
+        do {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, ring.size() + 1);
+            newRingNode = ring.get(randomNum);
+        }while (newRingNode == starEdge[1]);
+        starEdge[1] = newRingNode;
+        star.set(starNodeToSwitch, starEdge);
     }
 
     public ArrayList<Integer> getRing() {
@@ -29,4 +43,6 @@ public class Solution {
     public void setStar(ArrayList<Integer[]> star) {
         this.star = star;
     }
+
+
 }
