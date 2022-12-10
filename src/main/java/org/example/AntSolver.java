06e892f33d974ebf5d9ad683709ea4ab10715407
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2022-12-10 20:41:14 +0100

    Compute cost, visibility and pheromone

--- src/main/java/org/example/AntSolver.java
+++ src/main/java/org/example/AntSolver.java
@@ -18,12 +18,42 @@ public class AntSolver {
     public Solution initialize(AntSolver problem){
         ArrayList<Integer> ringNodes = new ArrayList<>();
         ArrayList<Integer[]> starNodes = new ArrayList<>();
+        float[][] visibility = new float[problem.nbrOfNodes][problem.nbrOfNodes];
+        float[][] pheromone = new float[problem.nbrOfNodes][problem.nbrOfNodes];
 
         //for the initial solution every node must be inside the ring.
         for (int i = 1; i <= nbrOfNodes; i++){
             ringNodes.add(i);
         }
-        return new Solution(ringNodes, starNodes);
+
+        //Compute the cost of the initial solution to compute the base pheromone
+        int cost = getCost(ringNodes, starNodes, problem.ringCosts, problem.starCosts);
+
+        //Update the visibility and pheromone matrix.
+        int j = 0;
+        while(j < ringNodes.size()){
+            visibility[ringNodes.get(j) - 1][ringNodes.get(j + 1) - 1] = 1/ringCosts[ringNodes.get(j) - 1][ringNodes.get(j + 1) - 1];
+            pheromone[ringNodes.get(j) - 1][ringNodes.get(j + 1) - 1] = 1/(cost * ringNodes.size());
+        }
+
+        return new Solution(ringNodes, starNodes, visibility, pheromone, cost);
+    }
+
+    public int getCost(ArrayList<Integer> ring, ArrayList<Integer[]> star, int[][] ringCosts, int[][] starCosts) {
+        int i = 0;
+        int totalCost = 0;
+
+        while (i < ring.size()){
+            totalCost += ringCosts[ring.get(i) - 1][ring.get(i+1) - 1];
+            i ++;
+        }
+        for (Integer[] integers : star) {
+            int starNode = integers[0];
+            int destNode = integers[1];
+            totalCost += starCosts[starNode - 1][destNode - 1];
+        }
+
+        return totalCost;
     }
 
     public Solution solve(){
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,32 +5,25 @@ import java.util.ArrayList;
 public class Solution {
     private ArrayList<Integer> ring;
     private ArrayList<Integer[]> star;
+    private float[][] visibility;
+    private float[][] pheromone;
+    private int cost;
+
     public Solution(ArrayList<Integer> ring, ArrayList<Integer[]> star) {
         this.ring = ring;
         this.star = star;
     }
 
-    public int getCost(Solution solution, int[][] ringCosts, int[][] starCosts) {
-        int i = 0;
-        int totalCost = 0;
-        ArrayList<Integer> ring = solution.getRing();
-        ArrayList<Integer[]> star = solution.getStar();
+    public Solution(ArrayList<Integer> ring, ArrayList<Integer[]> star, float[][] visibility, float[][] pheromone, int cost) {
+        this.ring = ring;
+        this.star = star;
+        this.visibility = visibility;
+        this.pheromone = pheromone;
+        this.cost = cost;
 
-        while (i < ring.size()){
-            totalCost += ringCosts[ring.get(i) - 1][ring.get(i+1) - 1];
-            i ++;
-        }
-        for (Integer[] integers : star) {
-            int starNode = integers[0];
-            int destNode = integers[1];
-            totalCost += starCosts[starNode - 1][destNode - 1];
-        }
-
-        return totalCost;
     }
-
-    public ArrayList<Integer> getRing() {
-        return ring;
+    public ArrayList<Integer> getRing(){
+        return this.ring;
     }
 
     public void setRing(ArrayList<Integer> ring) {
@@ -38,7 +31,7 @@ public class Solution {
     }
 
     public ArrayList<Integer[]> getStar() {
-        return star;
+        return this.star;
     }
 
     public void setStar(ArrayList<Integer[]> star) {
